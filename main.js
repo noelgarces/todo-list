ed6f4a7b245f153b0243326102439055937b9e71
Noel-Garces08 <noel.garces08@gmail.com>
2020-10-22 18:45:32 -0600

    Clean up javascript

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-// *==================== Selectors ====================
+// Selectors for new category form
 const newCategoryForm = document.querySelector('[data-new-category-form]');
 const newCategoryInput = document.querySelector('[data-new-category-input]');
 
+// Selector for categories container
 const categoriesContainer = document.querySelector('[data-categories]');
 
+// Selector for currently viewing
 const currentlyViewing = document.querySelector('[data-currently-viewing]');
 
+// Selector for new todo form
 const newTodoFormToggler = document.querySelector('[data-new-todo-toggler]');
 const newTodoForm = document.querySelector('[data-new-todo-form]');
 const newTodoSelect = document.querySelector('[data-new-todo-select]');
 const newTodoInput = document.querySelector('[data-new-todo-input]');
 
+// Selector for edit todo form
 const editTodoForm = document.querySelector('[data-edit-todo-form]');
 const editTodoSelect = document.querySelector('[data-edit-todo-select]');
 const editTodoInput = document.querySelector('[data-edit-todo-input]');
 
+// Selector for todos container
 const todosContainer = document.querySelector('[data-cards]');
 
-// *==================== Varaibles ====================
+// Local storage keys
 const LOCAL_STORAGE_CATEGORIES_KEY = 'LOCAL_STORAGE_CATEGORIES_KEY';
 const LOCAL_STORAGE_TODOS_KEY = 'LOCAL_STORAGE_TODOS_KEY';
-const LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY =
-	'LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY';
-let selectedCategoryId = localStorage.getItem(
-	LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY
-);
+const LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY = 'LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY';
 
-let categories =
-	JSON.parse(localStorage.getItem(LOCAL_STORAGE_CATEGORIES_KEY)) || [];
+let selectedCategoryId = localStorage.getItem(LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY);
+let categories = JSON.parse(localStorage.getItem(LOCAL_STORAGE_CATEGORIES_KEY)) || [];
 let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOS_KEY)) || [];
 
-// *==================== Event Listeneres ====================
 // EVENT: Add Category
 newCategoryForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const categoryName = newCategoryInput.value;
 	const isCategoryEmpty = !categoryName || !categoryName.trim().length;
+
 	if (isCategoryEmpty) {
 		return console.log('please enter a task');
 	}
@@ -59,6 +60,7 @@ categoriesContainer.addEventListener('click', (e) => {
 		} else {
 			selectedCategoryId = e.target.dataset.categoryId;
 		}
+
 		saveAndRender();
 	}
 });
@@ -69,9 +71,7 @@ categoriesContainer.addEventListener('change', (e) => {
 		const newCategoryColor = e.target.value;
 		const categoryId = e.target.parentElement.dataset.categoryId;
 
-		const categoryToEdit = categories.find(
-			(category) => category._id === categoryId
-		);
+		const categoryToEdit = categories.find((category) => category._id === categoryId);
 
 		categoryToEdit.color = newCategoryColor;
 
@@ -82,12 +82,9 @@ categoriesContainer.addEventListener('change', (e) => {
 // EVENT: Delete Selected Category
 currentlyViewing.addEventListener('click', (e) => {
 	if (e.target.tagName.toLowerCase() === 'span') {
-		categories = categories.filter(
-			(category) => category._id !== selectedCategoryId
-		);
+		categories = categories.filter((category) => category._id !== selectedCategoryId);
 		todos = todos.filter((todo) => todo.categoryId !== selectedCategoryId);
 		selectedCategoryId = null;
-
 		saveAndRender();
 	}
 });
@@ -110,13 +107,16 @@ newTodoFormToggler.addEventListener('click', function() {
 // EVENT: Add Todo
 newTodoForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+
 	todos.push({
 		_id: Date.now().toString(),
 		categoryId: newTodoSelect.value,
 		todo: newTodoInput.value,
 	});
+
 	newTodoSelect.value = '';
 	newTodoInput.value = '';
+
 	saveAndRender();
 });
 
@@ -131,17 +131,13 @@ todosContainer.addEventListener('click', (e) => {
 
 		editTodoForm.classList.add('active');
 
-		todoToEdit = todos.find(
-			(todo) => todo._id === e.target.dataset.editTodo
-		);
+		todoToEdit = todos.find((todo) => todo._id === e.target.dataset.editTodo);
 
 		editTodoSelect.value = todoToEdit.categoryId;
 		editTodoInput.value = todoToEdit.todo;
 	}
 	if (e.target.classList[1] === 'fa-trash-alt') {
-		const todoToDeleteIndex = todos.findIndex(
-			(todo) => todo._id === e.target.dataset.deleteTodo
-		);
+		const todoToDeleteIndex = todos.findIndex((todo) => todo._id === e.target.dataset.deleteTodo);
 
 		todos.splice(todoToDeleteIndex, 1);
 
@@ -172,15 +168,9 @@ function saveAndRender() {
 }
 
 function save() {
-	localStorage.setItem(
-		LOCAL_STORAGE_CATEGORIES_KEY,
-		JSON.stringify(categories)
-	);
+	localStorage.setItem(LOCAL_STORAGE_CATEGORIES_KEY,JSON.stringify(categories));
 	localStorage.setItem(LOCAL_STORAGE_TODOS_KEY, JSON.stringify(todos));
-	localStorage.setItem(
-		LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY,
-		selectedCategoryId
-	);
+	localStorage.setItem(LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY,selectedCategoryId);
 }
 
 function render() {
@@ -206,11 +196,9 @@ function render() {
 }
 
 function renderCategories() {
-	categoriesContainer.innerHTML += `<li class="sidebar-item" style="${
-		selectedCategoryId === 'null' || selectedCategoryId === null
-			? 'font-weight: 600'
-			: ''
-	}" data-category-id="">View All</li>`;
+	categoriesContainer.innerHTML += `
+	<li class="sidebar-item" style="${selectedCategoryId === 'null' || selectedCategoryId === null ? 'font-weight: 600' : ''}" data-category-id="">View All</li>
+	`;
 
 	categories.forEach(({ _id, category, color }) => {
 		categoriesContainer.innerHTML += `<li class="sidebar-item" style="${
